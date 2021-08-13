bb1cec14ba258b852acc617fec36b2af47071914
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-12 22:53:26 -0400

    updated Workout model

--- models/index.js
+++ models/index.js
@@ -1,27 +1,37 @@
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-
+    day: {
+        type: Date,
+        default: Date.now
     },
-    type: {
-
-    },
-    duration: {
-
-    },
-    distance: {
-
-    },
-    weight: {
-
-    },
-    sets: {
-
-    },
-    reps: {
-
-    }
+    exercises : [{
+        type: {
+            type: String, 
+            required: "Select a type of workout - resistance, or cardio"
+        },
+        name: {
+            type: String, 
+            trim: true,
+            required: "Enter a name for your workout"
+        },
+        duration: {
+            type: Number, 
+            required: "Please enter a duration"
+        },
+        distance: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        }
+    }]
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
