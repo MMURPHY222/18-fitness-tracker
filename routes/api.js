7d3ed0f1b96d8e009420e88ef057fc1c3351f75a
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-12 23:38:10 -0400

    added update and aggregate routes

--- routes/api.js
+++ routes/api.js
@@ -20,3 +20,31 @@ router.get("/api/workouts/range", (req, res) => {
         res.status(400).json(err);
       });
   });
+
+router.get("api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+})
+
+router.put("api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+})
