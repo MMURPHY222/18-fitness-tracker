07c20f10f098ec6792dc2cecf60a4fc88fb96c7a
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-13 10:09:16 -0400

    updated models to allow for easier export

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises : [{
+        type: {
+            type: String, 
+            required: "Select a type of workout - resistance, or cardio"
+        },
+        name: {
+            type: String, 
+            trim: true,
+            required: "Enter a name for your workout"
+        },
+        duration: {
+            type: Number, 
+            required: "Please enter a duration"
+        },
+        distance: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        }
+    }]
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+// const db = { Workout: Workout }
+// module.exports = db;
+
+module.exports = Workout;
