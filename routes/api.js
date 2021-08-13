883afe2403331ef2350f5a84bf7e00ab298f0e07
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-12 23:16:14 -0400

    get last 7 workouts range

--- routes/api.js
+++ routes/api.js
@@ -10,3 +10,13 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
       });
   });
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
